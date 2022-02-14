dbd627fedee35ffba46d8b9459d6c33261e43a4d
Alejandro Waldo S <alejandro.waldo5978@alumnos.udg.mx>
2022-02-14 09:33:58 -0600

    data working

--- filtro.py
+++ filtro.py
@@ -7,12 +7,12 @@ def initial_excel():
     new_excel = Workbook()
     
     # Add Headers
-    new_excel.worksheets[0]['A1'].value = 'Ciudad'
-    new_excel.worksheets[0]['B1'].value = 'Estado'
+    new_excel.worksheets[0]['A1'].value = 'ciudad'
+    new_excel.worksheets[0]['B1'].value = 'estado'
     new_excel.worksheets[0]['C1'].value = 'clave_Estado'
-    new_excel.worksheets[0]['D1'].value = 'Lada'
-    new_excel.worksheets[0]['E1'].value = 'Longitud'
-    new_excel.worksheets[0]['F1'].value = 'Pais'
+    new_excel.worksheets[0]['D1'].value = 'lada'
+    new_excel.worksheets[0]['E1'].value = 'cifras'
+    new_excel.worksheets[0]['F1'].value = 'pais'
     return new_excel
 
 
@@ -32,13 +32,15 @@ def handleSatet(keyState):
     dato  = getState(keyState)
     
     if dato:
-        print(f'Tuvimos exito con {dato}')
+        # Si lo encuentra el estado a la primera
         return dato
     else:
-        # print(f'Busqueda extensa de: {keyState}')
+        ''' 
+            Si no le agregamos el texto extra (Y ÁREA METROPOLITANA) despues de la clave de estado
+            Podemos encontrar JAL y JAL Y ÁREA METROPOLITANA 
+        '''
         partition = keyState.split(' ')
         dato = getState(partition[0])
-        print(f'Tuvimos exito con {dato}')
         return dato
 
 def getState(KeyState):
@@ -49,15 +51,16 @@ while contador <= columnas:
     fullCity = sheet[f'A{contador}'].value
     fullCity = handleCity(fullCity)
     nameState = handleSatet(fullCity[1].strip())
+    extPhone = sheet[f'C{contador}'].value.split(' ')[0]
     
     new_excel.worksheets[0][f'A{contador}'].value = fullCity[0]
     new_excel.worksheets[0][f'B{contador}'].value = nameState
     new_excel.worksheets[0][f'C{contador}'].value = fullCity[1]
     new_excel.worksheets[0][f'D{contador}'].value = sheet[f'B{contador}'].value
-    new_excel.worksheets[0][f'E{contador}'].value = sheet[f'C{contador}'].value
+    new_excel.worksheets[0][f'E{contador}'].value = extPhone
     new_excel.worksheets[0][f'F{contador}'].value = 'Mexico'
-    # print(contador)
+    
     contador += 1
     # time.sleep(2)
-    
+
 new_excel.save(f'phone_clean_data.xlsx')
